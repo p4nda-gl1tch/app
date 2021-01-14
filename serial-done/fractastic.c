d3e37c1b3c6d7995a89123d9e5a2af1ce0b35466
unknown <tutuianu.florin.cristian@gmail.com>
2021-01-14 21:14:18 +0200

    muie1

--- serial-done/fractastic.c
+++ serial-done/fractastic.c
@@ -13,10 +13,10 @@
 #define MANDELBROT_MODE 2
 
 
-void draw_fractal(int **fractal, char *name, int width, int height){
+void draw_fractal(int **fractal, int width, int height){
     int i, j;
 
-    FILE *f = fopen(name, "w");
+    FILE *f = fopen("out-julia", "w");
 
     fprintf(f, "P3 %d %d 255\n", width, height);
 
@@ -70,22 +70,15 @@ int main(int argc, char **argv) {
         const double y_max;
         const int max_iterations, color_multiplier; 
         const double d;
-        const char *name;
+        // const char *name;
       
         double tmp1, tmp2;
 
-        fscanf(file, "%d %d %d %d %d %d %d %d %f %f %d %c", width, height, x_min, x_max, y_min, y_max, max_iterations, color_multiplier, tmp1, tmp2, d, name);
-        // fscanf(file, "%d %d %d %d", x_min, x_max, y_min, y_max);
-        // fscanf(file, "%d", max_iterations);
-        // fscanf(file, "%d", color_multiplier);
-        // fscanf(file, "%f", tmp1);
-        // fscanf(file, "%f", tmp2);
+        fscanf(file, "%d %d %d %d %d %d %d %d %f %f %d", width, height, x_min, x_max, y_min, y_max, max_iterations, color_multiplier, tmp1, tmp2, d);
+
 
         const double c_re = mode == JULIA_MODE ? tmp1 : 0;
         const double c_im = mode == JULIA_MODE ? tmp2 : 0;
-    
-        // fscanf(file, "%d", d);
-        // fscanf(file, "%s", name);
 
         const double x_step = (x_max - x_min) / width;
         const double y_step = (y_max - y_min) / height;
