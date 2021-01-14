84490f92a2851a1fa418f914d47d775a3837978a
unknown <tutuianu.florin.cristian@gmail.com>
2021-01-14 21:40:52 +0200

    muie1

--- serial-done/fractastic.c
+++ serial-done/fractastic.c
@@ -42,6 +42,7 @@ int main(int argc, char **argv) {
         int mode = UNKNOWN_MODE;
         
         in_filename = argv[1];
+        printf("%s", argv[1]);
         printf("%c", in_filename);
         FILE *file = fopen(in_filename, "r");
 
@@ -76,7 +77,7 @@ int main(int argc, char **argv) {
       
         double tmp1, tmp2;
 
-        fscanf(file, "%d %d ", &width, &height);
+        // fscanf(file, "%d %d ", &width, &height);
        // printf("%lf %lf %lf %lf %lf %lf %lf %lf %lf %lf %d", width, height, x_min, x_max, y_min, y_max, max_iterations, color_multiplier, tmp1, tmp2, d);
 
         // const double c_re = mode == JULIA_MODE ? tmp1 : 0;
